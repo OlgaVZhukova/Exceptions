17440ce1de24ebc73e30fb047c107bcb077095a5
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-06-23 17:29:10 +0300

    ArrayIndexOutOfBoundsException is added

--- HW1/Task1.java
+++ HW1/Task1.java
@@ -3,18 +3,25 @@ package HW1;
 public class Task1 {
     public static void main(String[] args) {
         //System.out.println(firstMethod(3, 0)); // ArithmeticException: / by zero
-        System.out.println(secondMethod()); // NullPointerException: Cannot invoke "String.length()" because "a" is null
-
+        // System.out.println(secondMethod()); // NullPointerException: Cannot invoke "String.length()" because "a" is null
+        System.out.println(thirdMethod(10, 11, 1)); // ArrayIndexOutOfBoundsException: Index 11 out of bounds for length 10
     }
 
     /*
     private static int firstMethod(int a, int b) {
         return a / b;
     }
-    */
+    
     private static int secondMethod() {
         String a = null;
         return a.length();
+    }
+    */
+
+    private static int[] thirdMethod(int a, int b, int c) {
+        int[] arr = new int[a];
+        arr[b] = c;
+        return arr;
     }
 
 }
