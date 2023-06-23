3fd3e404373d96d3f79a24e41d99ef4dfe6b50d7
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-06-23 17:26:31 +0300

    NullPointerException is added

--- HW1/Task1.java
+++ HW1/Task1.java
@@ -2,12 +2,19 @@ package HW1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println(firstMethod(3, 0)); // ArithmeticException: / by zero
-        
+        //System.out.println(firstMethod(3, 0)); // ArithmeticException: / by zero
+        System.out.println(secondMethod()); // NullPointerException: Cannot invoke "String.length()" because "a" is null
+
     }
 
+    /*
     private static int firstMethod(int a, int b) {
         return a / b;
+    }
+    */
+    private static int secondMethod() {
+        String a = null;
+        return a.length();
     }
 
 }
