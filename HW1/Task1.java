eeb5eed81f304915058628c3988d71ea11e9adff
Olga Zhukova <olga.v.zhukoff@gmail.com>
2023-06-23 17:31:44 +0300

    uncommented code that was commented during running of exceptions

--- HW1/Task1.java
+++ HW1/Task1.java
@@ -2,12 +2,11 @@ package HW1;
 
 public class Task1 {
     public static void main(String[] args) {
-        //System.out.println(firstMethod(3, 0)); // ArithmeticException: / by zero
-        // System.out.println(secondMethod()); // NullPointerException: Cannot invoke "String.length()" because "a" is null
+        System.out.println(firstMethod(3, 0)); // ArithmeticException: / by zero
+        System.out.println(secondMethod()); // NullPointerException: Cannot invoke "String.length()" because "a" is null
         System.out.println(thirdMethod(10, 11, 1)); // ArrayIndexOutOfBoundsException: Index 11 out of bounds for length 10
     }
 
-    /*
     private static int firstMethod(int a, int b) {
         return a / b;
     }
@@ -16,7 +15,6 @@ public class Task1 {
         String a = null;
         return a.length();
     }
-    */
 
     private static int[] thirdMethod(int a, int b, int c) {
         int[] arr = new int[a];
